0a76991ed8807b8da9473392f22a3b008ea0d745
elson-k <njabuloelson@gmail.com>
2023-07-28 22:13:45 +0200

    Init Commit

---
+++ src/main/java/com/eviro/assessment/grad001/njabulokhoza/NjabulokhozaApplication.java
@@ -0,0 +1,13 @@
+package com.eviro.assessment.grad001.njabulokhoza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjabulokhozaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NjabulokhozaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eviro/assessment/grad001/njabulokhoza/NjabulokhozaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eviro.assessment.grad001.njabulokhoza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NjabulokhozaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
